07d02248f60c85b73dc5d431e525a96b3538b9de
thallheim <orobinhedlund@gmail.com>
2023-01-11 21:30:34 +0100

    Fix lua path

--- update_nvim.sh
+++ update_nvim.sh
@@ -6,7 +6,7 @@
 echo ":: Updating nvim configs..."
 cp ~/.config/nvim/init.lua ./nvim/init.lua
 echo ":: init.lua copied."
-cp -r ~/.config/nvim/lua/ ./nvim/lua/
+cp -r ~/.config/nvim/lua/ ./nvim/
 echo ":: config subfolder copied."
 echo ":: Done."
 echo ":! Don't forget to push whatever you changed, yah?"
