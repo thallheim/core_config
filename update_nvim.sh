858159520c7cff0afadbd82fec10553d1cb5ec73
thallheim <orobinhedlund@gmail.com>
2023-01-11 17:49:37 +0100

    Typo'd to root

--- update_nvim.sh
+++ update_nvim.sh
@@ -4,6 +4,6 @@
 # Copies from ~/.config/nvim/init.lua
 
 echo ":: Updating nvim config..."
-cp ~/.config/nvim/init.lua /init.lua
+cp ~/.config/nvim/init.lua ./init.lua
 echo ":: init.lua copied."
 echo ":! Don't forget to push whatever you changed, yah?"
