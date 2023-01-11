3814724c10eb5217c52e77d55a5defdb97faed67
thallheim <orobinhedlund@gmail.com>
2023-01-11 18:54:19 +0100

    Folderify script

--- update_nvim.sh
+++ update_nvim.sh
@@ -3,7 +3,10 @@
 # Copies current nvim init.lua to current dir and overwrites it.
 # Copies from ~/.config/nvim/init.lua
 
-echo ":: Updating nvim config..."
-cp ~/.config/nvim/init.lua ./init.lua
+echo ":: Updating nvim configs..."
+cp ~/.config/nvim/init.lua ./nvim/init.lua
 echo ":: init.lua copied."
+cp -r ~/.config/nvim/lua/ ./nvim/lua/
+echo ":: config subfolder copied."
+echo ":: Done."
 echo ":! Don't forget to push whatever you changed, yah?"
