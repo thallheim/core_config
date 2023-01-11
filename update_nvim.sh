e8236d1010c03302cf6ca46fc8283b137d265deb
thallheim <orobinhedlund@gmail.com>
2023-01-11 17:42:16 +0100

    Add nvim init getter

---
+++ update_nvim.sh
@@ -0,0 +1,9 @@
+!#bin/bash
+
+# Copies current nvim init.lua to current dir and overwrites it.
+# Copies from ~/.config/nvim/init.lua
+
+echo ":: Updating nvim config..."
+cp ~/.config/nvim/init.lua /init.lua
+echo ":: init.lua copied."
+echo ":! Don't forget to push whatever you changed, yah?"
