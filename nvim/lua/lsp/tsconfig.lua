4c4b7174d886905ba547d59362dce42381fc8af5
thallheim <orobinhedlund@gmail.com>
2023-01-16 21:24:37 +0100

    fix lsp_hover

--- nvim/lua/lsp/tsconfig.lua
+++
@@ -1,5 +0,0 @@
-on_attach = function(client, bufnr)
-	local opts = { noremap = true, silent = true }
-	-- Hover actions
-	vim.api.nvim_buf_set_keymap('n', '*', '<cmd>lua vim.lsp.buf.hover()<CR>', { buffer = bufnr })
-end
