6dc6ca40e2dabcf20422683ba7751ba4f0188d20
thallheim <orobinhedlund@gmail.com>
2023-01-20 11:52:03 +0100

    -titlebar from emacs

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -548,7 +548,7 @@ awful.rules.rules = {
      { rule = { class = "Brave"}, properties = { titlebars_enabled = false }},
      { rule = { class = "discord"}, properties = { titlebars_enabled = false }},
      { rule = { class = "obsidian"}, properties = { titlebars_enabled = false }},
-
+    { rule = { class = "Emacs" }, properties = { titlebars_enabled = false, focus = false }},
     -- Set Firefox to always map on the tag named "2" on screen 1.
     -- { rule = { class = "Firefox" },
     --   properties = { screen = 1, tag = "2" } },
